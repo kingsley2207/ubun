522068aa2d00bbd5e5b2d73ea083928f6f23c674
kingsley2207 <kingsley2207@gmail.com>
2022-08-03 16:02:20 +0700

    2nd cm

---
+++ goal_indicate/pub.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int8
+
+def talker():
+    pub = rospy.Publisher('goal_indicate', Int8, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        hello_str = 9
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
